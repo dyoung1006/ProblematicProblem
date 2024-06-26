12eb3ef85eaab0da565127e7bede2eab5202c1ee
Damien Young <dyoung1006@gmail.com>
2024-06-26 09:15:48 -0700

    logical error resolved switched username/activity

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -98,7 +98,7 @@ namespace ProblematicProblem
                     randomNumber = rng.Next(0, (activities.Count - 1));
                     randomActivity = activities[randomNumber];
                 }
-                Console.WriteLine($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");                
+                Console.WriteLine($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");                
                 cont = YesNoExpressions.ToTrueFalse(Console.ReadLine());
             } while (cont);
         }
